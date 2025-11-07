0fe3c2c8cf1430f03fe2ee890aa00776ef36bf75
miguelalejandro1991 <mibitech22@gmail.com>
2025-11-07 14:04:40 -0500

    auto

--- Clases/src/com/krakedev/TestAuto.java
+++ Clases/src/com/krakedev/TestAuto.java
@@ -7,17 +7,15 @@ public class TestAuto {
 		Auto auto1 = new Auto();
 		Auto auto2 = new Auto();
 		
-		auto1.marca ="TOYOTA";
-		auto1.anio = 2022;
-		auto1.precio= 20000.0;
+		auto1.setMarca("TOYOTA");
+		auto1.setAnio (2022);
+		auto1.setPrecio (20000.0);
 		
-		auto2.marca="JAC";
-		auto2.anio= 2023;
-		auto2.precio= 15000.0;
 		
-		System.out.println("auto1.marca:"+auto1.marca);
-		System.out.println("auto1.anio:"+auto1.anio);
-		System.out.println("auto1.precio:"+auto1.precio);
+		
+		System.out.println("auto1.marca:"+auto1.getMarca());
+		System.out.println("auto1.anio:"+auto1.getAnio());
+		System.out.println("auto1.precio:"+auto1.getPrecio());
 				
 		
 
---
+++ Clases/src/com/krakedev/TestFecha.java
@@ -0,0 +1,13 @@
+package com.krakedev;
+
+public class TestFecha {
+
+	public static void main(String[] args) {
+		Fecha fecha1 = new Fecha();
+		fecha1.setAnio(2021);
+		fecha1.setMes(01);
+		fecha1.setDia(30);
+		System.out.println(fecha1.getAnio());
+	}
+
+}
