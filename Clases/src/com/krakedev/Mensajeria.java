0d8c46f59faa6813ddcff920426afab8f28b0f5b
miguelalejandro1991 <mibitech22@gmail.com>
2025-11-07 16:49:54 -0500

    mensajeria

--- Clases/src/com/krakedev/Mensajeria.java
+++ Clases/src/com/krakedev/Mensajeria.java
@@ -4,6 +4,11 @@ public class Mensajeria {
 	public void saludar() {
 		System.out.println("hola mundo");
 	}
+	
+	public Mensajeria(String mensaje) {
+		System.out.println(mensaje);
+		
+	}
 	public void saludarAmigos(String nombre, String apellido) {
 		System.out.println("Hola  " +nombre+" "+apellido);
 	}
--- Clases/src/com/krakedev/TestMensajeria.java
+++ Clases/src/com/krakedev/TestMensajeria.java
@@ -3,7 +3,7 @@ package com.krakedev;
 public class TestMensajeria {
 
 	public static void main(String[] args) {
-		Mensajeria m = new Mensajeria();
+		Mensajeria m = new Mensajeria("Bienvenido a la mensajeria");
 		m.saludar();
 		m.saludarAmigos("miguel", "rodriguez");
 		
