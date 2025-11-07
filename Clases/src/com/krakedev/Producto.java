5ad372caace402cda7fe8c380acbc3cf7decbafe
miguelalejandro1991 <mibitech22@gmail.com>
2025-11-07 14:55:27 -0500

    producto

--- Clases/src/com/krakedev/Producto.java
+++ Clases/src/com/krakedev/Producto.java
@@ -1,10 +1,36 @@
 package com.krakedev;
 
 public class Producto {
-	String nombre;
-	String descripcion;
-	double precio;
-	int stockAnual;
+	private String nombre;
+	private String descripcion;
+	private double precio;
+	private int stockAnual;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public int getStockAnual() {
+		return stockAnual;
+	}
+	public void setStockAnual(int stockAnual) {
+		this.stockAnual = stockAnual;
+	}
+	
+	
 	
 
 }
--- Clases/src/com/krakedev/TestProducto.java
+++ Clases/src/com/krakedev/TestProducto.java
@@ -7,39 +7,39 @@ public class TestProducto {
 		Producto productoB = new Producto();
 		Producto productoC = new Producto();
 		
-		productoA.nombre="PARACETAMOL";
-		productoA.descripcion="MEDICAMENTO";
-		productoA.precio=5.00;
-		productoA.stockAnual=500;
+		productoA.setNombre("PARACETAMOL");
+		productoA.setDescripcion("MEDICAMENTO");
+		productoA.setPrecio(5.00);
+		productoA.setStockAnual(500);
 		
-		productoB.nombre="ALCOHOL";
-		productoB.descripcion="MEDICAMENTO";
-		productoB.precio=4.00;
-		productoB.stockAnual=1500;
+		productoB.setNombre("ALCOHOL");
+		productoB.setDescripcion("MEDICAMENTO");
+		productoB.setPrecio(4.00);
+		productoB.setStockAnual(1500);
 		
-		productoC.nombre="VITAMINAS";
-		productoC.descripcion="MEDICAMENTO";
-		productoC.precio=8.00;
-		productoC.stockAnual=2500;
+		productoC.setNombre("VITAMINAS");
+		productoC.setDescripcion("MEDICAMENTO");
+		productoC.setPrecio(8.00);
+		productoC.setStockAnual(2500);
 		
-		System.out.println("nombre:" +productoA.nombre);
-		System.out.println("descripcion:" +productoA.descripcion);
-		System.out.println("precio:" +productoA.precio);
-		System.out.println("stockAnual:" +productoA.stockAnual);
-		
-		System.out.println("----------------");
-		
-		System.out.println("nombre:" +productoB.nombre);
-		System.out.println("descripcion:" +productoB.descripcion);
-		System.out.println("precio:" +productoB.precio);
-		System.out.println("stockAnual:" +productoB.stockAnual);		
+		System.out.println("nombre:" +productoA.getNombre());
+		System.out.println("descripcion:" +productoA.getDescripcion());
+		System.out.println("precio:" +productoA.getPrecio());
+		System.out.println("stockAnual:" +productoA.getStockAnual());
 		
 		System.out.println("----------------");
 		
-		System.out.println("nombre:" +productoC.nombre);
-		System.out.println("descripcion:" +productoC.descripcion);
-		System.out.println("precio:" +productoC.precio);
-		System.out.println("stockAnual:" +productoC.stockAnual);
+		System.out.println("nombre:" +productoB.getNombre());
+		System.out.println("descripcion:" +productoB.getDescripcion());
+		System.out.println("precio:" +productoB.getPrecio());
+		System.out.println("stockAnual:" +productoB.getStockAnual());	
+		
+		System.out.println("----------------");
+		
+		System.out.println("nombre:" +productoC.getNombre());
+		System.out.println("descripcion:" +productoC.getDescripcion());
+		System.out.println("precio:" +productoC.getPrecio());
+		System.out.println("stockAnual:" +productoC.getStockAnual());
 		
 		
 		
